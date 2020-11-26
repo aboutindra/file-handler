46ecf0219e46a1ca941e53f98c69c76b57f315e9
Indra <me@aboutindra.com>
2020-11-27 01:18:54 +0700

    Upload data and convert excel to json

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const helpers = require('./helpers');
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
-const excelToJson = require('convert-excel-to-json');
-const fs = require('fs');
-
+const excelToJson = require("convert-excel-to-json");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -28,43 +24,24 @@ app.post("/image", (req, res) => {
     if(err) {
       res.status(400).send("Something went wrong!\nmsg : "+ err);
     }
-    var XLSX = require('xlsx');
-    var workbook = XLSX.readFile(req.file.path);
-    var sheet_name_list = workbook.SheetNames;
-    var data = [];
-    sheet_name_list.forEach(function(y) {
-      var worksheet = workbook.Sheets[y];
-      var headers = {};
-      for(z in worksheet) {
-        if(z[0] === '!') continue;
-        //parse out the column, row, and value
-        var tt = 0;
-        for (var i = 0; i < z.length; i++) {
-          if (!isNaN(z[i])) {
-            tt = i;
-            break;
-          }
-        };
-        var col = z.substring(0,tt);
-        var row = parseInt(z.substring(tt));
-        var value = worksheet[z].v;
-
-        //store header names
-        if(row == 1 && value) {
-          headers[col] = value;
-          continue;
-        }
-
-        if(!data[row]) data[row]={};
-        data[row][headers[col]] = value;
+    var result = excelToJson({
+      sourceFile: req.file.path,
+      header: {
+        rows: 1
+      },
+      columnToKey: {
+        '*': '{{columnHeader}}'
       }
-      //drop those first two rows which are empty
-      data.shift();
-      data.shift();
-      // console.log(data);
     });
+    var keys = Object.keys(result);
+    console.log(keys);
+    const hasil = [];
+    for(let i = 0; i < keys.length; i++){
+      hasil.push(result[keys[i]])
+    }
     const data2 = { status:false, codeRoom: req.body.code_room }
-    data.map(data => Object.assign(data, data2))
-    res.send({"file" : req.file, "excel" : data});
+    var finalData = [].concat.apply([], hasil);
+    finalData.map(data => Object.assign(data, data2))
+    res.send({"file" : req.file, "excel" : finalData});
   });
 });
